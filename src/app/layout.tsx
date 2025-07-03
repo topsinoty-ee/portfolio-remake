3d3ffe01d567f8d8e424ed8cc5eecad25948e127
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-07-03 15:54:05 +0300

    feat: add structured data and enhance metadata for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,48 @@ import { Navbar } from "~/components/layout/navbar";
 import { Footer } from "~/components/layout/footer";
 import { env } from "~/env";
 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Promise",
+  url: String(env.WEBSITE),
+  sameAs: [
+    "https://github.com/topsinoty-ee",
+    "https://www.linkedin.com/in/promise-temitope",
+    // Other shit
+  ],
+  jobTitle: "Frontend Developer",
+  address: {
+    "@type": "PostalAddress",
+    addressCountry: "Estonia",
+  },
+  alumniOf: {
+    "@type": "Organization",
+    name: "Tallinna Mustamäe Gümnaasium",
+  },
+  knowsAbout: [
+    "React",
+    "TypeScript",
+    "Tailwind CSS",
+    "JavaScript",
+    "Web Development",
+    "Git",
+    "Figma",
+    "Vitest",
+    "Postman",
+    "Apollo Client",
+    "MongoDB",
+    "Shadcn/ui",
+    "GraphQL",
+    "Angular",
+    "Flask",
+    "Python",
+    "Express.js",
+    "Java",
+    "nx",
+  ],
+};
+
 export const metadata: Metadata = {
   metadataBase: new URL(String(env.WEBSITE)),
   // title: "Promise Temitope | Portfolio",
@@ -23,6 +65,37 @@ export const metadata: Metadata = {
     siteName: "Promise Temitope",
   },
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: [
+    "topsinoty",
+    "promise",
+    "promise temitope",
+    "dev",
+    "developer",
+    "portfolio",
+    "Oluwatobiloba Promise Temitope",
+    "Frontend development",
+    "Promise",
+    "frontend developer",
+    "React developer",
+    "TypeScript developer",
+    "Tailwind CSS",
+    "JavaScript",
+    "web developer portfolio",
+    "Estonia developer",
+    "Next.js portfolio",
+    "open source contributor",
+    "creative frontend engineer",
+    "modern web apps",
+    "developer portfolio site",
+    "freelance frontend developer",
+    "UI engineer",
+    "Estonia web developer",
+    "Codesters.club",
+    ...jsonLd.knowsAbout,
+  ],
+  other: {
+    "script:ld+json": JSON.stringify(jsonLd),
+  },
 };
 
 const ubuntu = Ubuntu_Sans({
