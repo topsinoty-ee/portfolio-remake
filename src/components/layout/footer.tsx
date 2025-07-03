a8c6c6018ab2530b0eec79bcc628be64c27e5786
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-07-03 14:36:46 +0300

    refactor: update layout and footer components for portfolio branding and consistency

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -9,30 +9,21 @@ export const Footer = () => (
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-1">
           <SectionHeader className="w-max gap-0 select-none">
-            Devfolio<span className="text-secondary">.v25</span>
+            Portfolio&nbsp;<span className="text-secondary"> v2.4</span>
           </SectionHeader>
-          <p className="text-muted-foreground font-light">
-            Basic portfolio to show my skills
-          </p>
+          <p className="text-muted-foreground font-light">Basic portfolio to show my skills</p>
         </div>
         {/* <Socials /> */}
       </div>
       <Badge variant={"secondary"} className="rounded-full">
-        © 2025{" "}
-        <a href="https://github.com/topsinoty-ee/portfolio-2025">
-          @topsinoty-ee
-        </a>
-        . All rights reserved.
+        © 2025 <a href="https://github.com/topsinoty-ee/portfolio-2025">@topsinoty-ee</a>. All rights reserved.
       </Badge>
       {/* <LoginButton variant={"secondary"} />
       <LogoutButton variant={"destructive"} /> */}
     </div>
 
     <div className="hidden h-full w-1/2 items-end justify-end self-end bg-black md:flex">
-      <Badge
-        variant="outline"
-        className="drop-shadow-accent bg-card px-4 py-2.5 text-lg drop-shadow-xs"
-      >
+      <Badge variant="outline" className="drop-shadow-accent bg-card px-4 py-2.5 text-lg drop-shadow-xs">
         Find me online @topsinoty
       </Badge>
     </div>
