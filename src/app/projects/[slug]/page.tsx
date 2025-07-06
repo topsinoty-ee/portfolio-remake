7a8f45460d3fa3e5e6f02f61940d904b8292a574
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-07-06 13:29:23 +0300

    fix: update ProjectPage component to accept params as a Promise containing slug

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,3 @@
-export default function ProjectPage({ slug }: { slug: string }) {
+export default function ProjectPage({ params }: { params: Promise<{ slug: string }> }) {
   return <section className="flex h-full w-full flex-col justify-center gap-8"></section>;
 }
